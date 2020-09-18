9ea7c73edfae223b86f6c4a084e531015e1198de
Hai <nguyenlonghai1412@gmail.com>
2020-09-18 20:11:11 +0700

    hai ne

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -8,7 +8,7 @@ var SinhVien = function(masv, tensv, email, diemtoan, diemhoa, diemly, diemrenlu
     this.diemRenLuyen = diemrenluyen;
     this.loaiSinhVien = loaisinhvien;
     this.sinhVienHuy == sinhVienHuy;
-    this.sinhVienHai == sinhVienHai;
+    this.sinhVienHai == sinhVienKai;
     this.tinhDiemTrungBinh = () => {
         var dtb = (Number(this.diemHoa) + Number(this.diemLy) + Number(this.diemToan)) / 3;
         return dtb;
