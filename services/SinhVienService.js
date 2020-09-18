2b10d769388d69855fd433ce21d77dc719b2c059
Hai <nguyenlonghai1412@gmail.com>
2020-09-18 19:58:35 +0700

    file

---
+++ services/SinhVienService.js
@@ -0,0 +1,38 @@
+var SinhVienService = function() {
+    // Phương thức giao tiếp backend qua api => lấy thông tin sinh viên từ server về
+    this.layDanhSachSinhVien = function() {
+        var promise = axios({
+            url: 'http://svcy.myclass.vn/api/SinhVienApi/LayDanhSachSinhVien',
+            method: 'GET'
+        });
+        return promise;
+    }
+    this.xoaSinhVien = function( maSinhVien ) {
+        var promise = axios({
+            url: `http://svcy.myclass.vn/api/SinhVienApi/XoaSinhVien?maSinhVien=${maSinhVien}`,
+            method: 'DELETE'
+        })
+        return promise; 
+    }
+    this.layThongTinSinhVien = function( maSinhVien ) {
+        var promise = axios({
+            url: `http://svcy.myclass.vn/api/SinhVienApi/LayThongTinSinhVien?maSinhVien=${maSinhVien}`,
+            method: 'GET'
+        })
+        return promise; 
+    }
+    // Gửi cả 1 đối tượng sinh viên vào
+    this.capNhatThongTinSinhVien = function( maSinhVien, sinhVienUpdate ) {
+        var promise = axios({
+            url: `http://svcy.myclass.vn/api/SinhVienApi/CapNhatThongTinSinhVien?maSinhVien=${maSinhVien}`,
+            method: 'PUT',
+            data: sinhVienUpdate
+        })
+        return promise;
+    }
+    //Tính năng tìm kiếm sinh viên
+    this.timKiemSinhVien = function( keyword ) {
+        console.log('Chức năng tìm kiếm sinh viên');
+        return '';
+    }
+}
